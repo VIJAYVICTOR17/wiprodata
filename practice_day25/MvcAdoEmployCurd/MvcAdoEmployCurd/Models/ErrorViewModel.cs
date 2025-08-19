da4038eabeff66aefe803d5aac9687be6082f0f5
VIJAYVICTOR17 <victorvijay017@gmail.com>
2025-08-19 23:35:43 +0530

    code

---
+++ practice_day25/MvcAdoEmployCurd/MvcAdoEmployCurd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcAdoEmployCurd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ practice_day25/MvcDemo/MvcDemo/Models/Employ.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo.Models
+{
+    public class Employ
+    {
+        [Key]
+        [Required(ErrorMessage = "Please Enter Employ No")]
+        public int Empno { get; set; }
+
+        [MinLength(5, ErrorMessage = "Minimum 5 chars")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 chars")]
+        [Required(ErrorMessage = "Please Enter Name")]
+        public string? Name { get; set; }
+       
+        public int Basic { get; set; }
+    }
+}
