057890a715b1fd2d41e04b8efeca8b81a932e4ba
VIJAYVICTOR17 <victorvijay017@gmail.com>
2025-08-21 18:43:54 +0530

    code

---
+++ practice_day27/RestEmployCurd/Models/EmployDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Reflection.Emit;
+
+namespace RestEmployCurd.Models
+{
+    public class EmployDbContext : DbContext
+    {
+        public EmployDbContext(DbContextOptions<EmployDbContext> options) : base(options)
+        {
+        }
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employ>().ToTable("Employ");
+        }
+
+        public DbSet<Employ> Employees { get; set; }
+    }
+}
---
+++ practice_day27/RestEmployCurd/Services/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using RestEmployCurd.Models;
+
+namespace RestEmployCurd.Services
+{
+    public interface IApiService
+    {
+        Task<IEnumerable<Employ>> GetEmployAsync();
+        Task<Employ?> GetEmployByIdAsync(int id);
+        Task<string> CreateEmployAsync(Employ employ);
+    }
+}
